800e20bec132be97054b40ffa69cf34f5c57685a
hufeng <hufeng_cn@yeah.net>
2018-04-03 15:35:08 +0800

    enhancement retrieve data from tushare

--- backend/stock-extractor/src/main/java/com/bassoon/stockextractor/python/FetchingReport.py
+++ backend/stock-extractor/src/main/java/com/bassoon/stockextractor/python/FetchingReport.py
@@ -1,70 +1,123 @@
+import sys
 import tushare as ts
 import pandas as p
 import datetime,time,string
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, func, text
+from sqlalchemy.orm import sessionmaker
 
+#api_name:table_name
+mapTable = {
+    'get_profit_data':'stock_profit_data',
+    'get_operation_data':'stock_operation_data',
+    'get_growth_data':'stock_growth_data',
+    'get_debtpaying_data':'stock_debtpay_data',
+    'get_cashflow_data':'stock_cashflow_data',
+    # table for test
+    #'get_profit_data':'stock_profit_test_feng'
+}
 
 def getQuarterList(start_year):
-    #current = time.strftime('%Y%m',time.localtime(time.time()))
-    current = '201712'      #cannot retrieve the data of 2018, so from 201712
+    current = time.strftime('%Y%m',time.localtime(time.time()))
+    #current = '201712'      #cannot retrieve the data of 2018, so from 201712
     end_year = current[0:4]
     end_month= current[4:6]
     quarters = []
 
     if start_year > end_year:
-        print 'start year should older than current'
+        print('start year should older than current')
     else:
         for year in range(int(start_year),int(end_year)+1):
             end_quarter = 4
             if year == int(end_year):
-                end_quarter = int(end_month)/4 + 1
+                end_quarter = int(int(end_month)/4 + 1)
             for quarter in range(1,end_quarter + 1):
                 quarters.append(str(year)+ ('%02d'%quarter))
     return quarters
 
 def retrieveData(api_name,table_name,quarters):
+    mytable = getTableObject(table_name)
+    try:
+        year_last = session.query(func.max(mytable.c.year)).scalar()
+        quarter_last = session.query(func.max(mytable.c.quarter)).filter(mytable.c.year == year_last).scalar()
+        if year_last == None:
+            year_last = 0
+        if quarter_last == None:
+            quarter_last = 0
+    except:
+        year_last = 0
+        quarter_last = 0
+        print('Failed to retrieve Max of year and quarter, set them to zero')
+
+    print('Latest data in table: ' + table_name + ' is ', year_last, quarter_last)
+    if year_last > 10:
+        #back to 1 year make sure we can get newest data
+        year_last = year_last - 1
+
     for quarter in quarter_list:
-        year_int    = int(quarter[0:4])
-        quarter_int = int(quarter[4:6])
-        print 'Starting retrieve data of ' + api_name + ' of ',year_int, quarter_int
-        #continue
-        if api_name == 'profit':
-            report = ts.get_profit_data(year_int,quarter_int)
-        elif api_name == 'operation':
-            report = ts.get_operation_data(year_int,quarter_int)
-        elif api_name == 'growth':
-            report = ts.get_growth_data(year_int,quarter_int)
-        elif api_name == 'debtpaying':
-            report = ts.get_debtpaying_data(year_int,quarter_int)
-        elif api_name == 'cashflow':
-            report = ts.get_cashflow_data(year_int,quarter_int)
-        else:
-            print 'Tushare API:' + api_name + ' wrong'
-            break
-        report['year'] = p.Series(year_int,index=report.index)
-        report['quarter'] = p.Series(quarter_int,index=report.index)
-        report.to_sql(table_name, engine, if_exists='append')
-        print report
+        year_cur    = int(quarter[0:4])
+        quarter_cur = int(quarter[4:6])
+        if year_cur > year_last or (year_cur == year_last and quarter_cur >= int(quarter_last)):
+            print('Starting retrieve data of %s of %d %d '%(api_name, year_cur, quarter_cur))
+            #continue
+            try:
+                exec_api = 'ts.%s(%d,%d)'%(api_name,year_cur,quarter_cur)
+                print('exec Tushare API :%s'%exec_api)
+                #execute the tushare API call
+                report = eval(exec_api)
+                if year_last != 0:
+                    cnt = session.query(func.count(mytable.c.year)). \
+                        filter(mytable.c.year == year_cur). \
+                        filter(mytable.c.quarter == quarter_cur).scalar()
+                    if cnt != len(report.index):
+                        sql = 'delete from %s where year=%d and quarter=%d'%(table_name,year_cur,quarter_cur)
+                        session.execute(sql)
+                        session.commit()
+                        print('\ndelete old data of %d %d from %s'%(year_cur,quarter_cur,table_name))
+                    else:
+                        #no changes, do next
+                        print('\nno data is updated')
+                        continue
+                report['year'] = p.Series(year_cur,index=report.index)
+                report['quarter'] = p.Series(quarter_cur,index=report.index)
+                print(report)
+                report.to_sql(table_name, engine, if_exists='append')
+            except IOError:
+                print('\nIOError: might no data on server')
+
+def getTableObject(name):
+    metadata = MetaData()
+    table_object = Table(name,metadata,
+                         Column('year',String),
+                         Column('quarter',String))
+    return table_object
 
 #which year is the data retrieved from ?
 quarter_list = getQuarterList('2007')
 engine = create_engine('mysql://root:Liaobi()7595k@10.20.116.107/CN_BASSOON?charset=utf8')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+### It will clear tables if add the 'clear' to the command line
+### eg:   python3.6 FetchingReport.py clear
+if len(sys.argv) == 2:
+    if sys.argv[1] == 'clear':
+        print('remove data from all tables first')
+        for api,table in mapTable.items():
+            sql = 'delete from %s'%(table)
+            session.execute(sql)
+            session.commit()
+
+#Retrieve the data through the definition of mapTable
+for api,table in mapTable.items():
+    retrieveData(api,table,quarter_list)
 
 #ying li neng li
 #ts.get_profit_data(2014,3)
-retrieveData('profit','stock_profit_data',quarter_list)
 #ying yun nengli
 #ts.get_operation_data(2014,3)
-retrieveData('operation','stock_operation_data',quarter_list)
-
 #cheng zhang neng li
 #ts.get_growth_data(2014,3)
-retrieveData('growth','stock_growth_data',quarter_list)
-
 #chang zai neng li
 #ts.get_debtpaying_data(2014,3)
-retrieveData('operation','stock_debtpay_data',quarter_list)
-
 #xian jin liu liang
 #ts.get_cashflow_data(2014,3)
-retrieveData('cashflow','stock_cashflow_data',quarter_list)
