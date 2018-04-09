0fc5615998f09de81e84f006e221dff73bedc7b2
frankforp <frankforp@users.noreply.github.com>
2018-04-09 14:55:40 +0800

    delay·

--- frontend/stock-admin-new/build/js/indextop.js
+++ frontend/stock-admin-new/build/js/indextop.js
@@ -990,6 +990,32 @@ function init_addMap(){
 }
 
 $(document).ready(function () {
+    
+   $('.collapse-link').on('click', function () {
+        var $BOX_PANEL = $(this).closest('.x_panel'),
+            $ICON = $(this).find('i'),
+            $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+
+        // fix for some div with hardcoded fix class
+        if ($BOX_PANEL.attr('style')) {
+            $BOX_CONTENT.slideToggle(200, function () {
+                $BOX_PANEL.removeAttr('style');
+            });
+        } else {
+            $BOX_CONTENT.slideToggle(200);
+            $BOX_PANEL.css('height', 'auto');
+        }
+
+        $ICON.toggleClass('fa-chevron-up fa-chevron-down');
+    });
+
+    $('.close-link').click(function () {
+        var $BOX_PANEL = $(this).closest('.x_panel');
+
+        $BOX_PANEL.remove();
+    });
+    
+    
     console.log("load dada for index top")
     init_indextop();
     init_echartareapro();
