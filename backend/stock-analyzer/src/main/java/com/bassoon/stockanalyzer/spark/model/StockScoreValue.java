3e36a28fd3d3e2c16733f812aec68c5e993efdc6
xxu <xxu@rocketsoftware.com>
2018-04-09 16:54:18 +0800

    Add redis for performance improvement

--- backend/stock-analyzer/src/main/java/com/bassoon/stockanalyzer/spark/model/StockScoreValue.java
+++ backend/stock-analyzer/src/main/java/com/bassoon/stockanalyzer/spark/model/StockScoreValue.java
@@ -6,8 +6,8 @@ public class StockScoreValue implements Serializable {
     private String code;
     private String name;
     private int score = 0;
-    private FilterCondition filterCondition;//当前年份指标，跟查询年份相同
-    private FilterCondition previousFilterCondition;//去年的的指标
+    private FilterCondition filterCondition = new FilterCondition();//当前年份指标，跟查询年份相同
+    private FilterCondition previousFilterCondition = new FilterCondition();//去年的的指标
     private double change;//当年的涨幅，需要根据这个排序
 
     public String getCode() {
